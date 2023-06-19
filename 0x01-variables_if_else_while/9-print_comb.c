36e10aba0884c1152469090defa5639e447aded6
AlexLearnz <alexmwangi106@gmail.com>
2023-06-19 03:59:25 -0700

    0, 1, 2, ..., 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Expected output: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+ * putchar(number)
+ * putchar(',')
+ * putchar(' ')
+ *
+ * Loop from 48 to 57
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
