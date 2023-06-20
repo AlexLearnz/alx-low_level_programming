557d57bc249ba1a90e43dba99b49b65b6aa55c41
AlexLearnz <alexmwangi106@gmail.com>
2023-06-19 19:54:59 -0700

    2 digit combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * man ascii
+ *
+ * i=0-8, j=1-9
+ * i > j -> 01 == 10; 09 == 90
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	i = 48;
+	while (i <= 56)
+	{
+		j = 49;
+		while (j <= 57)
+		{
+			/* To avoid same combination */
+			if (j > i)
+			{
+				putchar(i);
+				putchar(j);
+				/* No comma & space in last num */
+				if (i != 56 || j != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			/* Increment j */
+			j++;
+		}
+		/* Increment i */
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
