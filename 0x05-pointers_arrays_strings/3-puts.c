b1a7015acb422684be0b359d7ac10d9ca54cef94
AlexLearnz <alexmwangi106@gmail.com>
2023-06-26 10:37:39 -0700

    Iterating each character until it reaches '\0'null terminator

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string + newline
+ * @str: The string to be printed
+ *
+ * Return: None
+ */
+void _puts(char *str)
+{
+	while(*str != 0)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar(10);
+}
