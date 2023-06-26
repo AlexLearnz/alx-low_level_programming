4c8e90b1af944878734ec5878de4763dbfb1c00b
AlexLearnz <alexmwangi106@gmail.com>
2023-06-26 07:26:28 -0700

    Using strlen function

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Return the length of a string
+ * @s: The characters present in that string
+ *
+ * Return: None
+ */
+int _strlen(char *s)
+{
+	return strlen(s);
+}
+
