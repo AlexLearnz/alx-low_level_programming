f4218941ad3f9aef4c9d3141f3ce241c4e94557c
AlexLearnz <alexmwangi106@gmail.com>
2023-06-26 06:22:19 -0700

    Getting started: int value reassignment using pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes a pointer to an int as parameter then \
+ * updates the value it points to 98
+ * @n: The int pointer
+ *
+ * Return: None
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Can pointers be used to efficiently store, use and create data?
