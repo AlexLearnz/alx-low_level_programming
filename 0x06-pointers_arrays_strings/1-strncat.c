cf85d137973f58a47c3bef4aed2c219189338068
AlexLearnz <alexmwangi106@gmail.com>
2023-06-29 19:49:28 -0700

    Concatenate n number of char from src

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,13 +1,27 @@
 #include "main.h"
 
 /**
- * _strncat - Concatenaes 2 strings
+ * _strncat - Concatenaes 2 strings,
+ * number of char/bytes to be appended from src are specified
  * @dest: Destination string
  * @src: Source string
- * @n: Number of bytes to be used from src
+ * @n: Number of bytes to be used from src ||
+ * n number of characters to be appended
  *
  * Return: Pointer to resulting string dest
  */
-char *_strncat(char *dest, *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
+	int i = 0;
+	int dest_len = 0;
 
+	/* Find length of dest */
+	while (dest[i++])
+		dest_len++;
+
+	/* Concatenate at most 'n' characters from src to dest */
+	for (i = 0; src[i] && i < n; i++)
+		dest[dest_len++] = src[i];
+
+	return (dest);
+}
