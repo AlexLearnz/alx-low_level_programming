398243ea963028719f45e8dd2663988782dd60e7
AlexLearnz <alexmwangi106@gmail.com>
2023-06-29 20:01:32 -0700

    Copy n characters from src to dest

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,6 +8,22 @@
  *
  * Return: Pointer to resulting string dest
  */
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n)
 {
+	int i = 0;
+	int src_len = 0;
 
+	/* Find len of src string */
+	while (src[i++])
+		src_len++;
+
+	/* Copy 'n' char from src to dest */
+	for (i = 0; src[i] && i < n; i++)
+		dest[i] = src[i];
+
+	/* Add the other remaining index positions with null */
+	for (i = src_len; i < n; i++)
+		dest[i] = 0;
+
+	return (dest);
+}
