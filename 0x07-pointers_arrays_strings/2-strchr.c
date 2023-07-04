10dc26c10cec0114a2f4ac03ddaad937f3fe72c2
AlexLearnz <alexmwangi106@gmail.com>
2023-07-03 19:13:28 -0700

    Perhaps null should be included in the loop?

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,11 +10,13 @@
 char *_strchr(char *s, char c)
 {
 	/* NULL is at the end of a C string, s points to start */
-	while (*s != 0)
+	while (*s >=  0)
 	{
 		if (*s == c)
+		{
 			/* Pointer to char c */
 			return (s);
+		}
 		s++;
 	}
 	/* Otherwise null */
