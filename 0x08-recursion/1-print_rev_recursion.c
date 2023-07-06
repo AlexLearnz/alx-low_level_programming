c119a40a66d9390b05264371cb50f90a9354ffc5
AlexLearnz <alexmwangi106@gmail.com>
2023-07-06 04:59:56 -0700

    print rev recursions

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,2 +1,17 @@
 #include "main.h"
 
+/**
+ * _print_rev_recursion(char *s)
+ * @s: The string
+ *
+ * Return: none
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
+
