3f75372038d741556478e0ce376795385fd5f8ab
AlexLearnz <alexmwangi106@gmail.com>
2023-07-06 04:49:20 -0700

    puts() recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,2 +1,20 @@
 #include "main.h"
 
+/**
+ * _puts_recursion - Prints string + newline
+ * @s: The string
+ *
+ * Return: None
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		_putchar(10);
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
