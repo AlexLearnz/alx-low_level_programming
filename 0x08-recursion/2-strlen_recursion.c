383a2d39312bc54084d4ce9395d7961bfd9b9d97
AlexLearnz <alexmwangi106@gmail.com>
2023-07-06 20:14:18 -0700

    Is it a prime number?

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,2 +1,15 @@
 #include "main.h"
 
+/**
+ * _strlen_recursion - Returns len of a str
+ * @s: The string
+ *
+ * Return: len
+ */
+int _strlen_recursion(char *s)
+{
+	while (*s)
+	{
+		_strlen_recursion(s + 1);
+	}
+}
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,2 +1,28 @@
 #include "main.h"
+/**
+ * is_prime_number - Tells if an integer is a prime number
+ * @n: Then input integer
+ *
+ * Checking all possibilities before...
+ * Concluding is_prime_number
+ *
+ * Return: 1, otherwise 0
+ */
+int is_prime_number(int n)
+{
+	/* Checking divisibility first, in order */
+	if (n <= 1)
+		return (0);
+	if (n % 2 == 0)
+		return (0);
+	if (n % 3 == 0)
+		return (0);
+	if (n % 5 == 0)
+		return (0);
+	if (n % 7 == 0)
+		return (0);
+	if (n % 1 == 0)
+		return (1);
+	return (is_prime_number(n));
+}
 
