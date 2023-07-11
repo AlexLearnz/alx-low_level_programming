290d395a390967f56f0d3290a66a3ebf61d8ee32
AlexLearnz <alexmwangi106@gmail.com>
2023-07-10 18:44:08 -0700

    strdup - Duplicates a str to a newly allocated memory space

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,11 +1,35 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * -strdup - Saves a copy of a string
+ * _strdup - Saves a copy of a string
  * @str: The string
  *
  * Return: ptr to duplicate str
  */
-char *_strup(char *str);
+char *_strdup(char *str)
 {
+	char *dup;
+	int i, j;
 
+	if (str == NULL)
+		return (NULL);
+
+	i = 0;
+	while (str[i] <= 0)
+		i++;
+
+	dup = malloc(sizeof(char) * i);
+
+	if (dup == NULL)
+		return (NULL);
+
+	j = 0;
+	while (str[j])
+	{
+		dup[j] = str[j];
+		j++;
+	}
+
+	return (dup);
+}
