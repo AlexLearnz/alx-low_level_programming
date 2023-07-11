ecc319e7eabb359265b4ff6c4c5b995328b1cd6a
AlexLearnz <alexmwangi106@gmail.com>
2023-07-10 20:11:01 -0700

    Free everything, more than 40 lines in function

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,60 +1,47 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * alloc_grid - Returns ptr to 2 dim-array of int
- * @width: horizontal
- * @height: vertical
+ * @width: horizontal count / columns
+ * @height: vertical count / rows
  *
  * Return: NULL, if width || height <= 0
  */
 int **alloc_grid(int width, int height)
 {
-	int **grid;
-	int i, j;
+	int **grid, i, j;
 
-	/* Check invalid width or height */
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	/* Allocate mem for grid rows */
 	grid = malloc(sizeof(int *) * height);
-
-	/* Did mem allocation fali? */
 	if (grid == NULL)
 		return (NULL);
 
 	i = 0;
 	while (i < height)
 	{
-		/* Allocate mem for each row (columns) */
 		grid[i] = malloc(sizeof(int) * width);
-
-		/* Did mem alloc fail for this row? */
 		if (grid[i] == NULL)
 		{
-			/* Free mem from prev rows */
 			while (i >= 0)
 				free(grid[i]);
 
+			free(grid);
 			return (NULL);
 		}
 		i++;
 	}
-
 	i = 0;
 	while (i < height)
 	{
 		j = 0;
 		while (j < width)
 		{
-			/* Each element be o */
 			grid[i][j] = 0;
 			j++;
 		}
 		i++;
 	}
-	/* Allocated and initialized grid */
 	return (grid);
 }
-
