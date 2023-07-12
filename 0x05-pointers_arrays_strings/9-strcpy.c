c793289f7a2d3e1b58b68a68f13df208ef814606
AlexLearnz <alexmwangi106@gmail.com>
2023-07-12 07:42:28 -0700

    Copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,6 +11,20 @@
  */
 char *_strcpy(char *dest, char *src)
 {
+	int i, j;
 
-	return (*dest);
+	i = 0;
+	/* src[i] == *(src + i) */
+	while (*(src + i))
+		i++;
+
+	j = 0;
+	while (j < i)
+	{
+		*(dest + j) = *(src + j);
+		j++;
+	}
+
+	*(dest + i) = 0;
+	return (dest);
 }
