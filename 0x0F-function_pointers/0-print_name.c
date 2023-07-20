90664d379587917c79300359f6a8aa61db44b99d
AlexLearnz <alexmwangi106@gmail.com>
2023-07-19 19:17:15 -0700

    Function ptr used to print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name
+ * @name: The name string
+ * @f: pointer to the function
+ *
+ * functions are written as func(arg/parameter)
+ * for this function pointer f, its argument is a str
+ * which is name
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == 0)
+		return;
+	/* 'else' is not generally useful after 'break' or 'return' */
+	f(name);
+}
+
 
