1a93707be3d0b14e96a39d44f6d4778f0d88d86d
AlexLearnz <alexmwangi106@gmail.com>
2023-08-02 13:54:19 -0700

    Prime number recursive using linear

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,36 @@
 #include "main.h"
 
 /**
+ * prime_number_finder - Finds if num is prime
+ * @n: The number(num)
+ * @test: Possible divisor to be checked
+ * until n is reached
+ *
+ * Return: 1(prime number), 0 otherwise
+ */
+int prime_number_finder(int n, int test)
+{
+	/* Base case: First prime is 2 */
+	if (n < 2)
+	{
+		return (0);
+	}
+
+	/* Base: All possibilities checked */
+	if (test == 1)
+	{
+		return (1);
+	}
+
+	if (n % test == 0)
+	{
+		return (0);
+	}
+
+	return (prime_number_finder(n, test - 1));
+}
+
+/**
  * is_prime_number - Checks if num is prime num
  * @n: The num(number) to be checked
  *
@@ -9,8 +39,6 @@
 
 int is_prime_number(int n)
 {
-	if (n < 2)
-	{
-		return (0);
-	}
-
+	/* Test possibilities n down to 1 */
+	return (prime_number_finder(n, n - 1));
+}
