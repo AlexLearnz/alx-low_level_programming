e7c6ced7ca176eb45fe61f74798b1e2569d91d29
AlexLearnz <alexmwangi106@gmail.com>
2023-08-07 01:17:28 -0700

    Create a string and initialize it with a specific char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,13 +2,40 @@
 
 /**
  * create_array - Creates an array of chars
- * and initializes with a specific char
+ * and initializes it with a specific char
  * @size: Size of the array
- * @c: The initial char
+ * @c: The specific char
+ *
+ * An array of chars is called a string
  *
  * Return: ptr to array, NULL otherwise
  */
 char *create_array(unsigned int size, char c)
 {
-	return (0);
+	char *ptr;
+	unsigned int i;
+
+	/* Input size is 0. 0*sizeof = 0 */
+	if (size == 0)
+	{
+		return ('\0');
+	}
+
+	/* memory address size of 1byte * size */
+	ptr = malloc(sizeof(char) * size);
+
+	/* memory address size == 0bytes */
+	if (ptr == 0)
+	{
+		return ('\0');
+	}
+
+	/* ptr points to first element in block */
+	for (i = 0; i < size; i++)
+	{
+		/* ptr[i] = specific char */
+		*(ptr + i) = c;
+	}
+
+	return (ptr);
 }
