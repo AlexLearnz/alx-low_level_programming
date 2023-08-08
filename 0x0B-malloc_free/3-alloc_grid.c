4773eae5f43260a68755050bc260801902d37ed2
AlexLearnz <alexmwangi106@gmail.com>
2023-08-08 07:10:08 -0700

    2 dimensional array of ints and ptr to ptr

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,12 +3,49 @@
 /**
  * alloc_grid - Creates a grid of 2-dimensiona
  * array of integers
- * @width: horizontal size of grid
- * @height: Vertical size of grid
+ * @width: horizontal size of grid by columns
+ * @height: Vertical size of grid by rows
  *
  * Return: ptr to grid, 0 otherwise
  */
 int **alloc_grid(int width, int height)
 {
-	return (0);
+	int **ptr;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (0);
+	}
+
+	ptr = malloc(sizeof(int) * height);
+
+	if (ptr == NULL)
+	{
+		return (0);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = malloc(sizeof(int) * width);
+
+		if (ptr[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(ptr[i]);
+
+			free(ptr);
+			return (0);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			ptr[i][j] = 0;
+		}
+	}
+
+	return (ptr);
 }
