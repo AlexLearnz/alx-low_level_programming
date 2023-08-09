e9d16febd5189e11bc658b2eb9fb38e387ab4a13
AlexLearnz <alexmwangi106@gmail.com>
2023-08-08 18:42:30 -0700

    If NULL treat as empty string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,14 +12,11 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 	int len1, len2, i, j;
 
-	if (s1 == NULL || s2 == NULL)
-	{
-		if (s1 != NULL)
-			return (s1);
-		if (s2 != NULL)
-			return (s2);
-		return ('\0');
-	}
+	/* NULL == emmpty string */
+	if (s1 == NULL)
+		s1 = "";
+	if (s1 == NULL)
+		s2 = "";
 
 	len1 = 0;
 	for (i = 0; s1[i] != '\0'; i++)
