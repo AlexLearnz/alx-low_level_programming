caa6efdceb408e93c08e08b8a4ecf09fad8ebd01
AlexLearnz <alexmwangi106@gmail.com>
2023-08-10 19:50:34 -0700

    realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _realloc - reallocates a memory block
+ * @ptr: ptr to mem prev allocated
+ * @old_size: previoulsy allocated mem size
+ * @new_size: newly allocated mem size
+ *
+ * Return: ptr to newly allocated space
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+}
