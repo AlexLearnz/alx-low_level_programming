0dd343d19709aa3dfbfa525df8b64cb198d3d9b1
AlexLearnz <alexmwangi106@gmail.com>
2023-08-11 19:06:45 -0700

    reallocation of a memory block

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,7 +1,9 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _realloc - reallocates a memory block
+ * use malloc && free
  * @ptr: ptr to mem prev allocated
  * @old_size: previoulsy allocated mem size
  * @new_size: newly allocated mem size
@@ -10,5 +12,32 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	void *new_ptr;
+	unsigned int copy_size;
 
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		return (NULL);
+
+	if (ptr != NULL)
+	{
+		if (new_size > old_size)
+			copy_size = old_size;
+		else
+			copy_size = new_size;
+
+		memcpy(new_ptr, ptr, copy_size);
+		free(ptr);
+	}
+
+	return (new_ptr);
 }
