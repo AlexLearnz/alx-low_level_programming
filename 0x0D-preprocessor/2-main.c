c1d6bfa785e8fa28f387082c9610d404cbe3c8a4
AlexLearnz <alexmwangi106@gmail.com>
2023-08-14 19:02:33 -0700

    C-preprocesso tasks done

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * prints file name using standard
+ * predefined Macro __FILE__ - string
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
