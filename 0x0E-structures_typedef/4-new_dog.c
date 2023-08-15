1075d41714d9478b7d82585b883cc45688586941
AlexLearnz <alexmwangi106@gmail.com>
2023-08-14 19:59:02 -0700

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,44 @@
+#include "dog.h"
+#include <string.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return: pointer to new dog (Success), NULL otherwise
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+	int len1, len2;
+
+	len1 = strlen(name);
+	len2 = strlen(owner);
+
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+
+	d->name = malloc(sizeof(char) * (len1 + 1));
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+
+	d->owner = malloc(sizeof(char) * (len2 + 1));
+	if (d->owner == NULL)
+	{
+		free(d);
+		free(d->name);
+		return (NULL);
+	}
+
+	strcpy(d->name, name);
+	strpy(d->owner, owner);
+	d->age = age;
+
+	return (d);
+}
